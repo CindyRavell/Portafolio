181a90eafec2f3e638eec22fc14e4b3c563cd611
Cindy Ravell <cindy.ravell@theksquaregroup.com>
2021-05-02 21:08:21 -0500

    menu_lap

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-document.getElementById("navToogle").addEventListener("click",function(){
-  
-    document.getElementById("nav_menu").classList.toggle("mostrar");
-  });
+/*===== MENU SHOW =====*/ 
+const showMenu = (navToogle, navMenu) =>{
+  const toggle = document.getElementById(navToogle),
+  nav = document.getElementById(navMenu)
+
+  if(toggle && nav){
+      toggle.addEventListener('click', ()=>{
+          nav.classList.toggle('show')
+      })
+  }
+}
+showMenu('navToogle','navMenu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav_link');   
+
+function linkAction(){
+/*Active link*/
+navLink.forEach(n => n.classList.remove('active'));
+this.classList.add('active');
+
+/*Remove menu mobile*/
+const navMenu = document.getElementById('navmenu')
+navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
